5360143b556082e650455f6f867f677d274bf407
Sagor Majomder <sagormajomder@gmail.com>
2025-10-08 16:59:40 +0600

    Refactor RatingStateChart component for improved responsiveness and formatting

--- src/components/AppDetailsPage/RatingStateChart.jsx
+++ src/components/AppDetailsPage/RatingStateChart.jsx
@@ -7,6 +7,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import { formatLargeNumber } from "../../utils/utils";
 
 export default function RatingStateChart({ ratings }) {
   const sortedRatings = ratings.toSorted((a, b) => {
@@ -17,7 +18,7 @@ export default function RatingStateChart({ ratings }) {
     <section className="pb-10">
       <h4 className="heading-4">Ratings</h4>
 
-      <div className="h-[31.25rem]">
+      <div className="h-[18.75rem] md:h-[25rem] lg:h-[31.25rem]">
         <ResponsiveContainer width="100%" height="100%">
           <ComposedChart
             layout="vertical"
@@ -25,10 +26,23 @@ export default function RatingStateChart({ ratings }) {
             margin={{ top: 40, right: 30, left: 0, bottom: 5 }}
           >
             <CartesianGrid stroke="#f5f5f5" />
-            <XAxis type="number" />
-            <YAxis dataKey="name" type="category" scale="band" />
+            <XAxis
+              type="number"
+              tick={{ fontSize: "0.75rem" }}
+              tickFormatter={(value) => formatLargeNumber(value)}
+            />
+            <YAxis
+              dataKey="name"
+              type="category"
+              scale="band"
+              tick={{
+                fontSize: "0.9rem",
+                fontWeight: 500,
+                dy: 0,
+              }}
+            />
             <Tooltip />
-            <Bar dataKey="count" barSize={35} fill="#FF8811" />
+            <Bar dataKey="count" barSize={35} maxBarSize={50} fill="#FF8811" />
           </ComposedChart>
         </ResponsiveContainer>
       </div>
