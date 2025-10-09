b8117de922acb5206d402dd010175cdedad7599d
Sagor Majomder <sagormajomder@gmail.com>
2025-10-09 07:57:57 +0600

    Enhance AppsHeading component to support sorting

--- src/components/AppsHeading.jsx
+++ src/components/AppsHeading.jsx
@@ -1,33 +1,53 @@
-export default function AppsHeading({ totalApp, search, onSearch }) {
+export default function AppsHeading({
+  totalApp,
+  search,
+  onSearch,
+  sortBy,
+  onSortBy,
+}) {
   return (
     <div className="mb-4 flex flex-col-reverse items-center justify-between gap-4 sm:flex-row">
       <h4 className="heading-4">({totalApp}) Apps Found</h4>
 
-      <label className="input">
-        <svg
-          className="h-[1em] opacity-50"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <g
-            strokeLinejoin="round"
-            strokeLinecap="round"
-            strokeWidth="2.5"
-            fill="none"
-            stroke="currentColor"
+      {onSearch ? (
+        <label className="input">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
           >
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.3-4.3"></path>
-          </g>
-        </svg>
-        <input
-          value={search}
-          onChange={(e) => onSearch(e.target.value)}
-          type="search"
-          className="grow"
-          placeholder="Search Apps"
-        />
-      </label>
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input
+            value={search}
+            onChange={(e) => onSearch(e.target.value)}
+            type="search"
+            className="grow"
+            placeholder="Search Apps"
+          />
+        </label>
+      ) : (
+        <select
+          value={sortBy}
+          onChange={(e) => onSortBy(e.target.value)}
+          className="select appearance-none"
+        >
+          <option value="select" disabled={true}>
+            Sort By Downloads
+          </option>
+          <option value="high">High-Low</option>
+          <option value="low">Low-High</option>
+        </select>
+      )}
     </div>
   );
 }
--- src/pages/InstallationPage.jsx
+++ src/pages/InstallationPage.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import AppsHeading from "../components/AppsHeading";
+import Container from "../components/Container";
+import SectionTitle from "../components/SectionTitle";
+
 export default function InstallationPage() {
-  return <div>InstallationPage</div>;
+  const [sortBy, setSortBy] = useState("select");
+  return (
+    <section className="py-14">
+      <Container style="space-y-10">
+        <SectionTitle
+          title="Your Installed Apps"
+          desc="Review all applications currently installed that were developed by us"
+        />
+        <AppsHeading totalApp="0" sortBy={sortBy} onSortBy={setSortBy} />
+      </Container>
+    </section>
+  );
 }
